a3616dc25042cee1ca2e94c5114f0bfd1d15a960
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-02 01:09:52 -0500

    created appel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -17,7 +17,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
-	static final int UNIT_SIZE = 25 ;
+	static final int UNIT_SIZE = 50 ;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
 	static final int DELAY = 75;
 	
@@ -66,10 +66,17 @@ public class GamePanel extends JPanel implements ActionListener{
 			g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
 			g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
 		}
+		g.setColor(Color.red);
+		g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 		
 	}
 	
 	public void newApple() {
+		
+		appleX=random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleX=random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+		
+		
 	}
 	
 	public void move() {
