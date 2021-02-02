ecca2a5de58cdc46d2524db92333f575ec71f952
nazimudheen267 <nazimudheen267@gmail.com>
2021-02-02 00:03:59 -0500

    created snake methods into the GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,20 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+	
+	GamePanel(){
+		
+		
+	}
+		
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -1,4 +1,12 @@
 
 public class SnakeMain {
-
+	
+	public static void main(String[] args) {
+		
+		new GameFrame();
+	
+		
+		
+	}
+	
 }
